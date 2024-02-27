144fd128f29641b5e7fa8c2266d1002a02a64423
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2024-02-27 11:03:10 -0500

    Input/output graph animations
    Building from the last scene I created a new scene that after displaying the linear and saturation regions, as well as ON and OFF, shows how graphs are correlated. Then, these graphs are moved and two more graphs representing the rough input and output are plotted. Note that there is no specific circuit topology yet so it doesn't really correspond to anything specific yet (the K'(W/L) is also inaccurate for typical MOSFETs). Additionally, there is no labelling yet and the code could be cleaner.

--- MOSFETGraphingScenes.py
+++ MOSFETGraphingScenes.py
@@ -128,7 +128,7 @@ class MOSFETGraphsSmallSignal(Scene):
 
         # set parameters
         kPrimeWL = 1
-        Vth = 0.4
+        Vth = 1
         
         # set parameters that are functions
         Vds = lambda t: 4 # constant for the moment # TODO this is done with the operating voltage right now
@@ -304,8 +304,320 @@ class MOSFETGraphsSmallSignal(Scene):
         )
         # run simulation time forward
         self.play(
-            ApplyMethod(simTime.increment_value, 10, rate_func=rate_functions.double_smooth),
-            run_time=2,
+            ApplyMethod(simTime.increment_value, 2*5*np.pi, rate_func=rate_functions.smooth),
+            run_time=5,
         )
 
         self.wait(4)
+
+class MOSFETGraphsSmallSignalInputOutput(Scene):
+
+    def construct(self):
+        # set up time 
+        simTime = ValueTracker(0)
+
+        # set parameters
+        kPrimeWL = 1# TODO make a realistic value
+        Vth = 1
+        
+        # set parameters that are functions
+        Vds = lambda t: 4 # constant for the moment # TODO this is done with the operating voltage right now
+        Vgs = lambda t: 2 + 0.05*np.sin(t) # TODO this does not have dependence on Vds as it actually does yet = 3
+        
+        # derived params
+        Vod = lambda t: Vgs(t) - Vth
+        
+        def DrainSourceToCurrent(Vds, t):
+            if (Vod(t)<0): # if off return zero
+                return 0
+            if (Vds<Vod(t)): # less than overdrive, use triode equation
+                return kPrimeWL*(Vod(t)*Vds-0.5*(Vds**2))
+            else: # in SAT region
+                return 0.5*kPrimeWL*((Vod(t))**2) # no lambda for now
+            
+        def GateSourceToSatCurrent(Vgs):
+            if (Vgs<Vth):
+                return 0
+            else:
+                return 0.5*kPrimeWL*((Vgs-Vth)**2)# no lambda for now
+
+        headroom = 0.1 # how much above the max at sat to show as a multiplier
+        ax1 = Axes(
+            x_range=[0, 3, 1],
+            y_range=[0, GateSourceToSatCurrent(3)*(1+headroom), 1],
+            tips=False,
+            axis_config={"include_numbers": True, "color": WHITE},
+            x_length=4,
+            y_length=4,
+        )
+        
+        ax1.move_to(LEFT*3)
+      
+        # graphing parameters
+        # get range of drain to source with params to plot properly
+        satValue = 0.5*kPrimeWL*((Vod(0))**2)
+        ax2 = Axes(
+            x_range=[0, 10], # TODO set numbers
+            y_range=[0, satValue*(1+headroom)],
+            tips=False,
+            axis_config={"include_numbers": True, "color": WHITE},
+            x_length=4,
+            y_length=4,
+        )
+        ax2.move_to(RIGHT*3)
+
+        # write axes
+        self.play(Write(ax1), Write(ax2))
+        
+        # TODO add stuff below
+        gateSourceGraph = ax1.plot(lambda Vgs: GateSourceToSatCurrent(Vgs), x_range=[0, 3], color=WHITE)
+        self.play(Write(gateSourceGraph), run_time=3)
+
+        # write curve for each
+        drainSourceGraph = ax2.plot(lambda Vds: DrainSourceToCurrent(Vds, simTime.get_value()), color=WHITE)
+        self.play(Write(drainSourceGraph), run_time=3)
+
+        # make divider/brace for this region. For the braces just use function values at time 0,
+        # because they will be removed before simTime starts going
+        satValue = DrainSourceToCurrent(Vod(0), 0)
+        linSatPoint = ax2.coords_to_point(Vod(0), satValue)
+        linSatRegionDividerLine = ax2.get_vertical_line(linSatPoint, line_config={"dashed_ratio": 0.85}, color=YELLOW)
+
+        linRegionLeftPoint = ax2.coords_to_point(0, satValue)
+        linearRegionBrace = BraceBetweenPoints(linRegionLeftPoint, linSatPoint, UP, color=RED_B)
+        linearRegionBraceText = linearRegionBrace.get_text("LIN")
+
+        satRegionRightPoint = ax2.coords_to_point(10, satValue) # TODO using 10 as end of the graph
+        satRegionBrace = BraceBetweenPoints(linSatPoint, satRegionRightPoint, UP, color=GREEN_B)
+        satRegionBraceText = satRegionBrace.get_text("SAT")
+        
+        # add braces for the linear and saturation regions
+        self.play(
+            Write(linSatRegionDividerLine), 
+            Write(linearRegionBrace),
+            Write(linearRegionBraceText),
+            Write(satRegionBrace),
+            Write(satRegionBraceText),
+        )
+
+        gateSourceBraceHeight = GateSourceToSatCurrent(3) # TODO magic number 3 representing x range limit for graph
+        onOffPoint = ax1.coords_to_point(Vth, gateSourceBraceHeight)
+        
+        onOffDividerLine = ax1.get_vertical_line(onOffPoint, line_config={"dashed_ratio": 0.85}, color=YELLOW)
+
+        offRegionLeftPoint = ax1.coords_to_point(0, gateSourceBraceHeight)
+        offRegionBrace = BraceBetweenPoints(offRegionLeftPoint, onOffPoint, UP, color=RED_B)
+        offRegionBraceText = offRegionBrace.get_text("OFF")
+
+        onRegionRightPoint = ax1.coords_to_point(3, gateSourceBraceHeight)
+        onRegionBrace = BraceBetweenPoints(onOffPoint, onRegionRightPoint, UP, color=GREEN_B)
+        onRegionBraceText = onRegionBrace.get_text("ON")
+
+        self.play(
+            Write(onOffDividerLine),
+            Write(offRegionBrace),
+            Write(offRegionBraceText),
+            Write(onRegionBrace),
+            Write(onRegionBraceText)
+        )
+        
+        self.wait(4) 
+        # remove braces 
+        self.play(
+            Unwrite(linSatRegionDividerLine), 
+            Unwrite(linearRegionBrace),
+            Unwrite(linearRegionBraceText),
+            Unwrite(satRegionBrace),
+            Unwrite(satRegionBraceText),
+            Unwrite(onOffDividerLine), # TODO make group for this stuff? this is too much
+            Unwrite(offRegionBrace),
+            Unwrite(offRegionBraceText),
+            Unwrite(onRegionBrace),
+            Unwrite(onRegionBraceText)
+        )
+
+        # TODO when doign small signal analysis and moving vertical point and dot around together use a VGroup?
+        # add operating region dots to the scene
+        # Vds shouldn't really vary, but if it does it is set up to be a function here
+        # TODO double use of simTime.get_value in DrainSourceToCurrent is bad
+        VdsOperatingPoint = ax2.coords_to_point(Vds(simTime.get_value()), DrainSourceToCurrent(Vds(simTime.get_value()), simTime.get_value())) # TODO organize these param numbers
+        VdsOperatingPointLine = ax2.get_vertical_line(VdsOperatingPoint, color=BLUE)
+        VdsOperationPointDot = Dot(VdsOperatingPoint, color=BLUE)
+
+        # updaters to move this dot up and down (and left and right if needed)
+        def updateVdsOperatingPointLine(line):
+            newVdsOperatingPoint = ax2.coords_to_point(Vds(simTime.get_value()), DrainSourceToCurrent(Vds(simTime.get_value()), simTime.get_value())) # TODO organize these param numbers
+            newVdsOperatingPointLine = ax2.get_vertical_line(newVdsOperatingPoint, color=BLUE)
+            line.become(newVdsOperatingPointLine)
+
+        def updateVdsOperatingPointDot(dot):
+            newVdsOperatingPoint = ax2.coords_to_point(Vds(simTime.get_value()), DrainSourceToCurrent(Vds(simTime.get_value()), simTime.get_value())) # TODO organize these param numbers
+            newVdsOperationPointDot = Dot(newVdsOperatingPoint, color=BLUE)
+            dot.become(newVdsOperationPointDot)
+
+        VdsOperatingPointLine.add_updater(updateVdsOperatingPointLine)
+        VdsOperationPointDot.add_updater(updateVdsOperatingPointDot)
+
+        # create dot for Vgs and move it with function given
+        # Vgs operating point voltage is defined at the top
+        VgsOperatingPoint = ax1.coords_to_point(Vgs(simTime.get_value()), GateSourceToSatCurrent(Vgs(simTime.get_value()))) # TODO organize these param numbers
+        VgsOperatingPointLine = ax1.get_vertical_line(VgsOperatingPoint, color=BLUE)
+        VgsOperatingPointDot = Dot(VgsOperatingPoint, color=BLUE)
+
+        def updateVgsOperatingPointLine(line):
+            newVgsOperatingPoint = ax1.coords_to_point(Vgs(simTime.get_value()), GateSourceToSatCurrent(Vgs(simTime.get_value())))
+            newVgsOperatingPointLine = ax1.get_vertical_line(newVgsOperatingPoint, color=BLUE)
+            line.become(newVgsOperatingPointLine)
+
+        def updateVgsOperatingPointDot(dot):
+            newVgsOperatingPoint = ax1.coords_to_point(Vgs(simTime.get_value()), GateSourceToSatCurrent(Vgs(simTime.get_value())))
+            newVgsOperatingPointDot = Dot(newVgsOperatingPoint, color=BLUE)
+            dot.become(newVgsOperatingPointDot)
+
+        # moves point along with function and time
+        VgsOperatingPointLine.add_updater(updateVgsOperatingPointLine)
+        VgsOperatingPointDot.add_updater(updateVgsOperatingPointDot)
+
+        # upating the corresponding drain to source graph
+        def updateDrainSourceGraph(graph): # TODO remove redundancy from the section when this is set up?
+            newDrainSourceGraph = ax2.plot(lambda Vds: DrainSourceToCurrent(Vds, simTime.get_value()), color=WHITE)
+            graph.become(newDrainSourceGraph)
+
+        # upating the corresponding gate to source graph (for when axes are moveds)
+        def updateGateSourceGraph(graph): # TODO remove redundancy from the section when this is set up?
+            newGateSourceGraph = ax1.plot(GateSourceToSatCurrent, color=WHITE)
+            graph.become(newGateSourceGraph)
+
+        # updaters for draing to source axes (to change graph)
+        drainSourceGraph.add_updater(updateDrainSourceGraph)
+        gateSourceGraph.add_updater(updateGateSourceGraph)
+
+        # add dots
+        self.play(
+            Write(VgsOperatingPointLine), 
+            Write(VgsOperatingPointDot),
+            Write(VdsOperationPointDot), 
+            Write(VdsOperatingPointLine)
+        )
+
+        # run simulation time forward
+        self.play(
+            ApplyMethod(simTime.increment_value, 2*5*np.pi, rate_func=rate_functions.smooth),
+            run_time=5,
+        )
+        simTime.set_value(0) # reset value so can be played with again later without messing up updater functions
+
+        self.wait(4)
+
+        # move and rescale axes
+        # TODO give more descriptive name to ax1
+        newAx1 = Axes(
+            x_range=[0, 2, 1], # shrink x range and corresponding y range
+            y_range=[0, GateSourceToSatCurrent(2)*(1+headroom)],
+            tips=False,
+            axis_config={"include_numbers": True, "color": WHITE},
+            x_length=3,
+            y_length=3,
+        )
+        newAx1.move_to(LEFT*3.5+UP*1.1)
+
+        newAx2 = Axes(
+            x_range=[0, 10], # TODO set numbers
+            y_range=[0, satValue*(1+headroom)],
+            tips=False,
+            axis_config={"include_numbers": True, "color": WHITE},
+            x_length=3,
+            y_length=3,
+        )
+        newAx2.move_to(RIGHT*3+UP*1.1)
+        self.play(ax1.animate.become(newAx1), ax2.animate.become(newAx2))
+
+        # TODO make setting up the responsive graphs a function that is done at the top so that they extra stuff can be compartimentalizse better
+        # TODO potential reordering here? NOTE it actually worked changing it before playing because the update functions only work with the graphs
+        # move plots 
+        # TODO tinker with plot sizes so they can move up and out of the way for teh input and output signal to have enough space
+        # TODO this another wiggle is unneeded
+
+        # add the little small signal graphs at the bottom that respond to wiggle as well
+
+        # adding small signal graph for gate to source
+        inputSignalAxes = Axes(
+            x_range=[0, 10], # TODO set numbers
+            y_range=[1.7, 2.3, 0.1], # TODO make it set dynamically
+            tips=False,
+            axis_config={"include_numbers": True, "color": YELLOW},
+            x_length=5,
+            y_length=3,
+        )
+        inputSignalAxes.move_to(LEFT*4+DOWN*2)
+        inputSignalVgsGraph = inputSignalAxes.plot(Vgs, color=BLUE)
+        self.play(Write(inputSignalAxes), Write(inputSignalVgsGraph))
+        
+        inputSignalPoint = inputSignalAxes.coords_to_point(0, Vgs(0))
+        inputSignalDot = Dot(inputSignalPoint, color=RED) # TODO make a VGroup for these so that is functions better?
+        inputSignalVerticalLine = inputSignalAxes.get_vertical_line(inputSignalPoint, color=RED)
+
+        # TODO make these updater functions under a larger function since they are all so similar for the dots and the vertical lines
+        def updateInputSignalDot(dot):
+            newInputSignalPoint = inputSignalAxes.coords_to_point(simTime.get_value(), Vgs(simTime.get_value()))
+            newInputSignalDot = Dot(newInputSignalPoint, color=RED)
+            dot.become(newInputSignalDot)
+        
+        def updateInputSignalLine(line):
+            newInputSignalPoint = inputSignalAxes.coords_to_point(simTime.get_value(), Vgs(simTime.get_value()))
+            newInputSignalLine = inputSignalAxes.get_vertical_line(newInputSignalPoint, color=RED)
+            line.become(newInputSignalLine)
+        
+        # TODO get below working
+        inputSignalDot.add_updater(updateInputSignalDot)
+        inputSignalVerticalLine.add_updater(updateInputSignalLine)
+
+        self.play(Write(inputSignalDot), Write(inputSignalVerticalLine))
+
+        # adding output graph 
+        outputSignalAxes = Axes(
+            x_range=[0, 10], # TODO set numbers
+            y_range=[0.3, 0.7, 0.1], # TODO make it set dynamically
+            tips=False,
+            axis_config={"include_numbers": True, "color": YELLOW},
+            x_length=5,
+            y_length=3,
+        )
+        outputSignalAxes.move_to(RIGHT*2.5+DOWN*2)
+        outputSignalVdsGraph = outputSignalAxes.plot(lambda time: DrainSourceToCurrent(Vds(time), time), color=BLUE)
+        self.play(Write(outputSignalAxes), Write(outputSignalVdsGraph))
+
+        outputSignalPoint = outputSignalAxes.coords_to_point(0, DrainSourceToCurrent(Vds(0), 0))
+        outputSignalDot = Dot(outputSignalPoint, color=RED)
+        outputSignalLine = outputSignalAxes.get_vertical_line(outputSignalPoint, color=RED)
+
+        # TODO make updaters not so redundant
+        def updateOutputSignalDot(dot):
+            newOutputSignalPoint = outputSignalAxes.coords_to_point(simTime.get_value(), DrainSourceToCurrent(Vds(simTime.get_value()), simTime.get_value()))
+            newOutputSignalDot = Dot(newOutputSignalPoint, color=RED)
+            dot.become(newOutputSignalDot)
+
+        def updateOutputSignalLine(line):
+            newOutputSignalPoint = outputSignalAxes.coords_to_point(simTime.get_value(), DrainSourceToCurrent(Vds(simTime.get_value()), simTime.get_value()))
+            newOutputSignalLine = outputSignalAxes.get_vertical_line(newOutputSignalPoint, color=RED)
+            line.become(newOutputSignalLine)
+        
+        outputSignalDot.add_updater(updateOutputSignalDot)
+        outputSignalLine.add_updater(updateOutputSignalLine)
+        
+        self.play(Write(outputSignalDot), Write(outputSignalLine))
+        self.play(
+            ApplyMethod(simTime.increment_value, 8, rate_func=rate_functions.linear),
+            run_time=5,
+        )
+        simTime.set_value(0) # reset for playing again later
+
+        # add updaters to the graphs so they go forward as inputs 
+
+        # TODO make these updaters less redundant?
+        # TODO this does not work right now
+        def updateInputSignalVgsGraph(graph):
+            newInputSignalVgsGraph = inputSignalAxes.plot(lambda t: Vgs(t), x_range=[0, simTime.get_value()], color=BLUE) 
+            graph.become(newInputSignalVgsGraph)
+
+        inputSignalVgsGraph.add_updater(updateInputSignalVgsGraph)
